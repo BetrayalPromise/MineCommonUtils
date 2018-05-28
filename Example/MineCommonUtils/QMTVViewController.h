5e4da4dd5dbaeaac647a79428ae56a2aab68fab7
BetrayalPromise <BetrayalPromise@gmail.com>
2018-05-28 21:54:13 +0800

    Initial commit

---
+++ Example/MineCommonUtils/QMTVViewController.h
@@ -0,0 +1,13 @@
+//
+//  QMTVViewController.h
+//  MineCommonUtils
+//
+//  Created by BetrayalPromise on 05/28/2018.
+//  Copyright (c) 2018 BetrayalPromise. All rights reserved.
+//
+
+@import UIKit;
+
+@interface QMTVViewController : UIViewController
+
+@end
---
+++ Example/MineCommonUtils/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MineCommonUtils
+//
+//  Created by BetrayalPromise on 05/28/2018.
+//  Copyright (c) 2018 BetrayalPromise. All rights reserved.
+//
+
+@import UIKit;
+#import "QMTVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QMTVAppDelegate class]));
+    }
+}
